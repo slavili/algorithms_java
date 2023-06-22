a52bf59a8875578bdb51f3560dda91eb7064ce6d
Vilisov Vyacheslav <slavili@list.ru>
2023-06-22 16:35:00 +0700

    Заготовка для двусвязного списка

---
+++ Task003/src/main/java/program.java
@@ -0,0 +1,7 @@
+public class program {
+    public static void main(String[] args) {
+
+        userList u1 = new userList();
+      
+    }
+}
---
+++ Task003/src/main/java/userList.java
@@ -0,0 +1,15 @@
+/*
+Необходимо реализовать метод разворота связного списка (двухсвязного или односвязного на выбор).
+*/
+public class userList {
+    Node head;
+    Node tail;
+
+
+    public class Node{
+        int value;
+        Node next;
+        Node previous;
+    }
+
+}
