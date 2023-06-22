588bc1561aebc3ffb09a108030a7ebd4a0dacc74
Vilisov Vyacheslav <slavili@list.ru>
2023-06-22 16:46:24 +0700

    Реализован метод add - Добавление элемента в список

--- Task003/src/main/java/program.java
+++ Task003/src/main/java/program.java
@@ -2,6 +2,9 @@ public class program {
     public static void main(String[] args) {
 
         userList u1 = new userList();
-      
+
+        u1.add(10);
+        u1.add(11);
+        u1.add(12);
     }
 }
--- Task003/src/main/java/userList.java
+++ Task003/src/main/java/userList.java
@@ -5,6 +5,20 @@ public class userList {
     Node head;
     Node tail;
 
+    public void add(int value){
+        Node curNode = new Node();
+        curNode.value = value;
+        if(head == null){
+            head = curNode;
+            tail = curNode;
+        } else {
+            tail.next = curNode;
+            curNode.previous = tail;
+            tail = curNode;
+        }
+    }
+
+
 
     public class Node{
         int value;
